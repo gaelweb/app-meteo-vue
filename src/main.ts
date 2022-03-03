b599ecbf964de334ae3fcf848e894fef793cb2db
gaelMon <gael.monbrault@ovianet.com>
2022-03-03 17:04:32 +0100

    import pinia module on app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
+import "./index.css";
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
-
+app.use(pinia);
 app.mount("#app");
+
+// serialize, escape (VERY important if the content of the state can be changed
+// by the user, which is almost always the case), and place it somewhere on
+// the page, for example, as a global variable.
+//devalue(pinia.state.value);
