bd0bfece1999968d31545bd488961571a905e3cd
gaelMon <gael.monbrault@ovianet.com>
2022-03-03 17:03:30 +0100

    home page update - add meteo components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import SearchBarMeteo from "../components/searchbar-meteo.vue";
+import CardMeteo from "../components/card-meteo.vue";
+import { useMeteoStore } from "../stores/meteo.js";
+
+const meteoStore = useMeteoStore();
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- METEO SEARCHBAR-->
+    <SearchBarMeteo />
+    <!-- METEO CARD -->
+    <CardMeteo :meteo-data-api="{ meteoData }" />
+    {{ meteoStore.getData }}
   </main>
 </template>
