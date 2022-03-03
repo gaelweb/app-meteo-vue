ac601535d9bc90e8ee02806957daa3c4013dd84d
gaelMon <gael.monbrault@ovianet.com>
2022-03-03 17:06:20 +0100

    add card meteo component

---
+++ src/components/card-meteo.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    meteoDataApi: Object,
+  },
+  setup(props) {
+    props.meteoDataApi;
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>Meteo card</h1>
+    <p v-if="{ meteoDataApi }">
+      {{ meteoDataApi }}
+    </p>
+  </div>
+</template>
